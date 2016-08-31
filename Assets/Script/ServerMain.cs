0a17490a425953ff7682336066b48a845a2de839
unknown <red96146@naver.com>
2016-08-31 17:36:30 +0900

    2016.08.31 서버 유니티 프로젝트 타입으로 재 구성

---
+++ Assets/Script/ServerMain.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Net;
+using System.Net.Sockets;
+
+public class ServerMain : MonoBehaviour
+{
+	[SerializeField] Queue receiveCheckPoint;
+	[SerializeField] Queue sendCheckPoint;
+
+	void Awake()
+	{
+		receiveCheckPoint = new Queue();
+		sendCheckPoint = new Queue();
+
+
+	}
+}
